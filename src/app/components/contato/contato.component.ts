36beb7d618ec8a11ebe17bef6f48fd62f2b0ddee
NandoALves <fernandosaweyr@gmail.com>
2025-06-28 13:44:03 -0300

    Finalização da Lp

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+
 
 @Component({
   selector: 'app-contato',
-  imports: [],
+  standalone:true,
+  imports: [NgxMaskDirective],
+  providers: [provideNgxMask()],
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css', './rescontato.component.css']
 })
